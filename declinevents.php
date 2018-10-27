df32abfe5a983bffb2b391a94838836b981254bf
athiraas4981 <42322857+athiraas4981@users.noreply.github.com>
2018-10-27 14:28:06 +0530

    staff moduleE
    reject events

---
+++ declinevents.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$con=mysqli_connect("localhost","root","","mca");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$result = mysqli_query($con,"SELECT * FROM event WHERE status='2'");
+echo "<table border='2'>
+<tr>
+<th>EVENTS</th>
+<th> CONTENT</th>
+<th>IMAGE/POSTER</th>
+<th>TIME</th>
+<th>DATE</th>
+<th>VENUE</th>
+<th>COLLEGE</th>
+<th>PASS</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" .$row[1] . "</td>";
+echo "<td>" .$row[2] . "</td>";
+echo '<td><img src="data:image/jpeg;base64,'.base64_encode( $row[3] ).'" width="100px" height="100px"></td>';
+echo "<td>" .$row[4] . "</td>";
+echo "<td>" .$row[5] . "</td>";
+echo "<td>" .$row[6] . "</td>";
+//echo "<td>" . $row['image'] . "</td>";
+
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
+</body>
+</html>
