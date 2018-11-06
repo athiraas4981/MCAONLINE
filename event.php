006ad15e38ee1c65797be3855aea4074debf4616
jasminejoseph123 <43871107+jasminejoseph123@users.noreply.github.com>
2018-11-06 14:40:01 +0530

    events add
    events added

--- event.php
+++ event.php
@@ -23,8 +23,8 @@ $m=mysql_query($v) or die(mysql_error());
 <title>EVENTS</title>
 <title> MCA Student's Site</title>
 <meta charset="utf-8">
-<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
-<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/reset_st.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style_st.css" type="text/css" media="all">
 <script type="text/javascript" src="js/jquery-1.4.2.min.js" ></script>
 <script type="text/javascript" src="js/cufon-yui.js"></script>
 <script type="text/javascript" src="js/cufon-replace.js"></script>
