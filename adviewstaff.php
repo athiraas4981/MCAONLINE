4e961cc2f35f776249312942477dc77ea520176d
athiraas4981 <42322857+athiraas4981@users.noreply.github.com>
2018-11-06 08:24:24 -0800

    admin module modified
    view selected staff details

--- adviewstaff.php
+++ adviewstaff.php
@@ -6,7 +6,7 @@ include('config4.php');
 <meta charset="utf-8">
 <title>Untitled Document</title>
 </head>
-<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
 
 
 <body>
@@ -56,21 +56,51 @@ $sql="SELECT * FROM addclg";
 			</tr>
 		</table>
 	</form>
-</body>
-</html>
-<?php
+	
+	<?php
 if(isset($_POST['submit']))
 {
 	
-	$v1=$_POST['text'];
-	$v2=$_POST['code'];
-	$v3=$_POST['textarea'];
-	$v4=$_POST['district'];
-	$v5=$_POST['pin'];
-	$sql="INSERT INTO addclg(clgname,clgcode,address,district,pincode) Values('$v1','$v2','$v3','$v4','$v5')";
-    mysql_query($sql) OR die(mysql_error());
-	echo"<script> alert('inserted successfully'); </script>";
+	$clg=$_POST['district'];
+	?>
+	
+	<table>
+	<tr>
+	<td>Name</td>
+	<td>Email</td>
+	<td>Contact Number</td>
+	<td>College name</td>
+	</tr>
+	<?php
+	$sql="SELECT * FROM staff WHERE college_name='$clg'";
+		$q=mysql_query($sql) OR die(mysql_error());
+		$rc=mysql_num_rows($q);
+		if($rc==0)
+		{
+			echo error;
+		}
+		else
+		{
+			while($r=mysql_fetch_array($q)){
+			?>
+		<tr>
+			<td><?php echo $r[2]  ?></td>
+			<td><?php echo $r[4] ?></td>
+			<td><?php echo $r[3] ?></td>
+			<td><?php echo $r[1] ?></td>
+		</tr>
+		<?php
+			}
+		}
+		?>
+	</table>
+	
+	<?php
+	
 }
 ?>
+</body>
+</html>
+
 
 	
