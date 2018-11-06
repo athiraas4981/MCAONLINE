d2f8b44f8364021a0dccefd1c558ef51e0eb414c
athiraas4981 <42322857+athiraas4981@users.noreply.github.com>
2018-11-05 22:33:04 -0800

    admin module modified
    bootstrap is added in viewclg

--- viewclg.php
+++ viewclg.php
@@ -7,7 +7,7 @@ include('config4.php');
 <meta charset="utf-8">
 <title>Untitled Document</title>
 </head>
-
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 <body>
 	<table border="4" align="center">
 	<tr>
