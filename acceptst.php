de8ea0571d4042d6350ad8bdb05987f7f474d7d6
athiraas4981 <42322857+athiraas4981@users.noreply.github.com>
2018-11-12 11:18:21 +0530

    staff modified
    redirect to staffnot.php when the staff accept the student

--- acceptst.php
+++ acceptst.php
@@ -3,7 +3,8 @@ include('config.php');
 $id=$_REQUEST['id'];
 
 mysql_query("UPDATE st_reg SET status='1' WHERE id='$id'");
-
 mysql_query("UPDATE login SET status='1' WHERE st_id='$id' AND actype='student'");
+//echo "meta http-equiv='refresh' content='0;url=eventlist.php'>";
+echo "<script>alert('Successfully approved!'); window.location.replace('staffnot.php');</script>";
 
 ?>
