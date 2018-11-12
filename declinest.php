021984cd53f3c0c3f6133c046bbd065df7481bd2
athiraas4981 <42322857+athiraas4981@users.noreply.github.com>
2018-11-12 11:21:31 +0530

    staff modified
    redirect to staffnot.php when decline the registered students

--- declinest.php
+++ declinest.php
@@ -5,5 +5,6 @@ $id=$_REQUEST['id'];
 mysql_query("UPDATE st_reg SET status='2' WHERE id='$id'");
 
 mysql_query("UPDATE login SET status='2' WHERE st_id='$id' AND actype='student'");
-
+//echo "meta http-equiv='refresh' content='0;url=eventlist.php'>";
+echo "<script>alert('Successfully rejected!'); window.location.replace('staffnot.php');</script>";
 ?>
