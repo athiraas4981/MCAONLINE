460ead9c1f5bdb52033d447fbd02c5b952962fe7
jasminejoseph123 <43871107+jasminejoseph123@users.noreply.github.com>
2018-11-19 00:20:00 +0530

    modified notes
    view and download the available notes

--- login.php
+++ login.php
@@ -1,49 +1,70 @@
 <?php
-include("config.php");
- session_start();
-if(isset($_POST['submit']))
-{
-	
-$name=$_POST['email'];
-$pass=$_POST['password'];
-$_SESSION['name']=$name;
-$j="SELECT * FROM admin WHERE username='$name' and password='$pass' ";
-$v=mysql_query($j) or die(mysql_error());
-$s=mysql_num_rows($v);
-	if($s==0)
-	{
-		echo "<script>alert('username and password wrong');</script>";
-	}
-	else {
-	header('Location:index.html');
-	
-	}
-}
+session_start();
+include('config.php');
 ?>
 <!doctype html>
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="main.css">
 <meta charset="utf-8">
-<title>login</title>
+<title>Untitled Document</title>
 </head>
 
 <body>
 <div class="loginbox">
 <img src=".admin3.png" class="admin">
-
-    <form action="" method="post">
-        <h1>Login </h1>
-    <p>username</p>
-<input type="text" name="email" placeholder="Enter Username" id=""/>
-<p>password</p>
-        
-        <input type="text" name="password" placeholder="Enter password" id=""/>
-<input type="submit" name="submit" value="Login" placeholder="" id="Login"/>
-        <a href="#"><p>Forget password??</p></a>
+<form action="" method="post">
+	<input type="text" name="uname" placeholder="username">
+	<input type="password" name="pass" placeholder="password">
+	<input type="submit" name="submit" id="Login">
+    <a href="#"><p>Forget password??</p></a>
         <a href="http://www.pscexams.com/"><p>Don't have an account?</p></a>
-    </form>
-
-    </div>
+</form>
+</div>
 </body>
 </html>
+<?php
+if(isset($_POST['submit'])){
+	
+	$uname=$_POST['uname'];
+	$pass=$_POST['pass'];
+	
+	$sql="SELECT * FROM login WHERE email='$uname' AND password='$pass' AND status='1'";
+	
+	$qry=mysql_query($sql) or die(mysql_error());
+	
+	$rw=mysql_num_rows($qry);
+	
+	if($rw>0){
+		
+		while($row=mysql_fetch_array($qry)){
+			
+			
+		$actype=$row[4];
+		$id=$row[1];
+			
+			if($actype=="staff"){
+				
+				$_SESSION['uname']=$email;
+				$_SESSION['uid']=$id;
+				echo "<script>window.location.replace('index.html');</script>";
+				
+				
+			}elseif($actype=="student"){
+				
+				$_SESSION['uid']=$id;
+				
+				echo "<script>window.location.replace('student-prof.php');</script>";
+			}
+			
+			
+		}
+		
+		
+	}else{
+		
+		echo "invalid user";
+		
+	}
+}
+?>
